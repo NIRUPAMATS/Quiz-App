76177e0c11518b2fc903939443810f3bb9bab9f8
Nirupama T S <nirupamasajeevan@gmail.com>
2022-08-13 22:32:16 +0530

    second

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React,{useState} from "react";
+
+export default function App(){
+  const questions=[
+    {
+			questionText: 'What is the capital of France?',
+			answerOptions: [
+				{ answerText: 'New York', isCorrect: false },
+				{ answerText: 'London', isCorrect: false },
+				{ answerText: 'Paris', isCorrect: true },
+				{ answerText: 'Dublin', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Who is CEO of Tesla?',
+			answerOptions: [
+				{ answerText: 'Jeff Bezos', isCorrect: false },
+				{ answerText: 'Elon Musk', isCorrect: true },
+				{ answerText: 'Bill Gates', isCorrect: false },
+				{ answerText: 'Tony Stark', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'The iPhone was created by which company?',
+			answerOptions: [
+				{ answerText: 'Apple', isCorrect: true },
+				{ answerText: 'Intel', isCorrect: false },
+				{ answerText: 'Amazon', isCorrect: false },
+				{ answerText: 'Microsoft', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'How many Harry Potter books are there?',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '4', isCorrect: false },
+				{ answerText: '6', isCorrect: false },
+				{ answerText: '7', isCorrect: true },
+			],
+		},
+
+  ];
+  const [currentQ,setCurrentQ]=useState(0)
+  const [showScore,setShowScore]=useState(false)
+  const [score,setScore]=useState(0)
+  const handleOnClick=(isCorrect)=>{
+    if(isCorrect=== true){
+      setScore(score+1)
+    }
+    const nextQ=currentQ+1;
+    if(nextQ<questions.length)
+    setCurrentQ(nextQ)
+    else
+    setShowScore(true)
+  }
+  return (
+    <div className="app">
+      {showScore ? (
+      <div className='score-section'>You scored {score} out of {questions.length}</div>):(
+     <>
+        <div className="question-section">
+          <div className="question-count">
+            <span>Question {currentQ+1}</span>/{questions.length}
+          </div>  
+          <div className="question-text">{questions[currentQ].questionText}</div>
+        </div> 
+        <div className="answer-section">
+        {questions[currentQ].answerOptions.map((answerOption)=>(
+          <button onClick={()=>(handleOnClick(answerOption.isCorrect))}>{answerOption.answerText}</button>
+        ))}
+        </div> 
+
+     </>
+     )}
+    </div>
+  );
+
+}
